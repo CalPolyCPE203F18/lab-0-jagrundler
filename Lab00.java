28b502865aad9ca5bd8165790c88cb4947207bf1
jagrundler <josie@grundlerklan.com>
2018-09-24 13:30:12 -0700

    finished Lab00

--- Lab00.java
+++ Lab00.java
@@ -1,8 +1,45 @@
+import java.lang.*;
+
 public class Lab00
 {
    public static void main(String[] args)
    {
+        // declaring and initializing variables
+        int x = 5;
+        String y = "hello";
+        double z = 9.8;
+        
+        // printing the variables
+        System.out.println("x: " + x + " y: " + y + " z: " + z);
 
-   
-   }
+        // makimg an array
+        int[] nums = {3, 6, -1, 2};
+        for (int i = 0; i < nums.length; i++) {
+            System.out.println(nums[i]);
+        }
+
+        // calling a function
+        int numFound = charCount(y, 'l');
+        System.out.println("Found: " + numFound);
+
+        // counting for loop
+        for (int i = 1; i < 11; i++) {
+            System.out.print(i + " ");
+        }
+
+        System.out.println();
+    }
+
+    public static int charCount(String s, char c)
+    {
+        int count = 0;
+        // System.out.println(c);
+        // System.out.println(s);
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == c) {
+                count += 1;
+            }
+        }
+        return count;
+    }
 }
---
+++ javaTrial.java
@@ -0,0 +1,6 @@
+public class javaTrial {
+    public static void main(String [] args) {
+        String testString = "Hello Java";
+        System.out.println(testString);
+    }
+}
